b1fac49aadfb5f2817e79f66f25974a050b7db84
amhossaini <amhossaini@yahoo.com>
2023-02-16 12:47:39 +0330

    V2.0

--- decencol-BackEnd/routes/user.js
+++ decencol-BackEnd/routes/user.js
@@ -11,8 +11,14 @@ router.post('/getNonce', controller.getNonce);
 router.post('/loginWithPublicKey', controller.loginWithPublicKey);
 router.post('/createFile', auth.auth, controller.createFile);
 router.get('/getMyFiles', auth.auth, controller.getMyFiles);
-router.post('/shareFile', auth.auth, controller.shareFile);
+router.get('/getSharedByFiles', auth.auth, controller.getSharedByFiles);
+router.get('/getSharedList/:id', auth.auth, controller.getSharedList);
+router.post('/shareFile',  auth.auth, controller.shareFile);
+router.post('/removeShare',  auth.auth, controller.removeShare);
+router.post('/deleteFile',  auth.auth, controller.deleteFile);
 router.get('/getFile/:id', auth.auth, controller.getFile);
 router.post('/editFile', auth.auth, controller.editFile);
+router.post('/review', auth.auth, controller.review);
+
 
 module.exports = router;
